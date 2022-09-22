8c75a4b2a663d5079b788e21462af334b0f245f9
pradeeplingam <46870713+pradeeplingam@users.noreply.github.com>
2022-09-22 16:18:53 +0530

    Delete dudeapp-common directory

--- dudeapp-common/src/main/java/in/dudeapp/common/constant/MDCConstants.java
+++
@@ -1,9 +0,0 @@
-package in.dudeapp.common.constant;
-
-/**
- * Created by mohan on 16/07/22
- */
-public interface MDCConstants {
-
-    String REQUEST_ID = "requestID";
-}
--- dudeapp-common/src/main/java/in/dudeapp/common/exception/ErrorType.java
+++
@@ -1,6 +0,0 @@
-package in.dudeapp.common.exception;
-
-public enum ErrorType {
-	USER_SERVICE, COLLEGE_SERVICE, AWS_LAMBDA, UNKNOWN;
-
-}
